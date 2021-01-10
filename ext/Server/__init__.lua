530c2e919123cf7470d415fcd52214eacf23570c
Reirei Mael <10390711+rierei@users.noreply.github.com>
2021-01-10 14:21:38 -0300

    remove: dev debuging stuff

--- ext/Server/__init__.lua
+++ ext/Server/__init__.lua
@@ -5,8 +5,6 @@ local SoldierAppearances = require('__shared/soldiers-appearances')
 local WeaponAppearances = require('__shared/weapons-appearances')
 local WeaponUnlocks = require('__shared/weapons-unlocks')
 
-local BotsCustom = require('__shared/bots-custom')
-
 function ElementalFight:__init()
     self:RegisterVars()
     self:RegisterEvents()
@@ -32,10 +30,6 @@ function ElementalFight:RegisterVars()
 end
 
 function ElementalFight:RegisterEvents()
-    Events:Subscribe('Player:Chat', function (p_player, p_mask, p_message)
-        self:_PlayerChat(p_player, p_mask, p_message)
-    end)
-
     Events:Subscribe('Player:Respawn', function (p_player)
         self:_PlayerRespawn(p_player)
     end)
@@ -43,49 +37,6 @@ function ElementalFight:RegisterEvents()
     Hooks:Install('Soldier:Damage', 1, function (p_hook, p_soldier, p_info, p_giver)
         self:_SoldierDamage(p_hook, p_soldier, p_info, p_giver)
     end)
-end
-
--- spawning test bots
-function ElementalFight:_PlayerChat(p_player, p_mask, p_message)
-    if self.m_verbose >= 1 then
-        print('Event Player:Chat')
-    end
-
-    local s_USAssaultKit = ResourceManager:SearchForInstanceByGuid(Guid('A15EE431-88B8-4B35-B69A-985CEA934855'))
-    local s_USEngiKit = ResourceManager:SearchForInstanceByGuid(Guid('0A99EBDB-602C-4080-BC3F-B388AA18ADDD'))
-    local s_USReconKit = ResourceManager:SearchForInstanceByGuid(Guid('BC1C1E63-2730-4E21-8ACD-FAC500D720C3'))
-    local s_USSupportKit = ResourceManager:SearchForInstanceByGuid(Guid('47949491-F672-4CD6-998A-101B7740F919'))
-
-    local s_RUAssaultKit = ResourceManager:SearchForInstanceByGuid(Guid('28EC16E7-0BBF-4CB0-9321-473C6EC54125'))
-    local s_RUEngiKit = ResourceManager:SearchForInstanceByGuid(Guid('DB0FCE83-2505-4948-8661-660DD0C64B63'))
-    local s_RUReconKit = ResourceManager:SearchForInstanceByGuid(Guid('84A4BE20-B110-42E5-9588-365643624525'))
-    local s_RUSupportKit = ResourceManager:SearchForInstanceByGuid(Guid('4D0F249B-4464-4F97-A248-A88F57EF5CAA'))
-
-    local s_soldier1 = BotsCustom.spawnBot('Bot1', s_USAssaultKit, Vec3(-306.493164, 70.434372, 270.194336))
-    local s_soldier2 = BotsCustom.spawnBot('Bot2', s_USEngiKit, Vec3(-306.493164, 70.434372, 272.194336))
-    local s_soldier3 = BotsCustom.spawnBot('Bot3', s_USReconKit, Vec3(-306.493164, 70.434372, 274.194336))
-    local s_soldier4 = BotsCustom.spawnBot('Bot4', s_USSupportKit, Vec3(-306.493164, 70.434372, 276.194336))
-
-    local s_soldier5 = BotsCustom.spawnBot('Bot5', s_RUAssaultKit, Vec3(-306.493164, 70.434372, 278.194336))
-    local s_soldier6 = BotsCustom.spawnBot('Bot6', s_RUEngiKit, Vec3(-306.493164, 70.434372, 280.194336))
-    local s_soldier7 = BotsCustom.spawnBot('Bot7', s_RUReconKit, Vec3(-306.493164, 70.434372, 282.194336))
-    local s_soldier8 = BotsCustom.spawnBot('Bot8', s_RUSupportKit, Vec3(-306.493164, 70.434372, 284.194336))
-
-    local s_element = self:GetElement()
-    self:CustomizePlayer(s_soldier1.player, s_element)
-    self:CustomizePlayer(s_soldier5.player, s_element)
-
-    s_element = self:GetElement()
-    self:CustomizePlayer(s_soldier2.player, s_element)
-    self:CustomizePlayer(s_soldier6.player, s_element)
-
-    s_element = self:GetElement()
-    self:CustomizePlayer(s_soldier3.player, s_element)
-    self:CustomizePlayer(s_soldier7.player, s_element)
-
-    local s_element = self:GetElement()
-    self:CustomizePlayer(s_soldier4.player, s_element)
-    self:CustomizePlayer(s_soldier8.player, s_element)
 end
 
 -- customising player on respawn
